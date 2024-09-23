567906cccd0b97037caf1952fe64514bdd7e932d
João Praia <joaopraia@joaopraia.com.br>
2024-09-23 03:58:10 -0300

    aula-1

---
+++ .history/js/modulos/buscarPensamentos_20240922173650.js
@@ -0,0 +1,22 @@
+import api from '../api/api.js';
+import ui from './ui.js';
+
+async function buscarPensamentos() {
+	const elementos = {
+		buscaPensamento: '[data-js="busca-pensamento"]',
+	};
+
+	const buscaPensamento = document.querySelector(elementos.buscaPensamento);
+	const termoDaBusca = buscaPensamento.value;
+
+	try {
+		const pensamentosFiltrados = await api.buscarPensamentosPorTermo(termoDaBusca);
+		ui.renderizarPensamentos(pensamentosFiltrados);
+	} catch (erro) {
+		alert('Erro ao buscar pensamentos', erro);
+		console.error('Erro em buscar pensamentos', erro);
+		throw erro;
+	}
+}
+
+export default buscarPensamentos;
---
+++ .history/js/modulos/ui_20240922184915.js
@@ -0,0 +1,51 @@
+import api from './api.js';
+import constroiPensamento from './constroiPensamento.js';
+import formularioPreenchidoComPensamento from './formularioPreenchidoComPensamento.js';
+import mensagemDeMuralVazio from './mensagemDeMuralVazio.js';
+
+const ui = {
+	async renderizarPensamentos(pensamentosFiltrados = null) {
+		const listaPensamentos = document.querySelector('[data-js="lista-de-pensamentos"]');
+		const freagmento = document.createDocumentFragment();
+
+		try {
+			let pensamentosParaRenderizar;
+
+			if (Array.isArray(pensamentosFiltrados) && pensamentosFiltrados.length > 0) {
+				pensamentosParaRenderizar = pensamentosFiltrados;
+			} else {
+				pensamentosParaRenderizar = await api.buscarPensamentos();
+			}
+
+			if (pensamentosParaRenderizar.length === 0) {
+				mensagemDeMuralVazio();
+				return;
+			}
+
+			pensamentosParaRenderizar.forEach(ui.adicionarPensamentoNaLista);
+		} catch (error) {
+			console.log('Erro em renderizar pensamentos', error);
+		}
+	},
+
+	adicionarPensamentoNaLista(pensamento) {
+		const listaPensamentos = document.querySelector('[data-js="lista-de-pensamentos"]');
+		listaPensamentos.appendChild(constroiPensamento(pensamento));
+	},
+
+	async editarPensamentoDaLista(pensamentoId) {
+		const pensamento = await api.buscarPensamentosPorId(pensamentoId);
+		formularioPreenchidoComPensamento(pensamento);
+	},
+
+	async excluirPensamentoDaLista(pensamentoId) {
+		try {
+			await api.excluirPensamento(pensamentoId);
+			ui.renderizarPensamentos();
+		} catch (error) {
+			console.log('Erro em excluir pensamento', error);
+		}
+	},
+};
+
+export default ui;
